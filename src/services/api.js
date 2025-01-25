8ad74eed3808c1e78f3e8f9e04d7dbb3da2527e9
Jeremy21032 <jeremisma2001@hotmail.com>
2025-01-25 13:41:34 -0500

    Se cambia el metodo de api

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,10 @@ import config from "../config";
 
 const api = axios.create({
   baseURL: config.API_URL, // URL del backend
+  headers: {
+    "Content-Type": "application/json"
+  },
+  withCredentials: false
 });
 
 export default api;
